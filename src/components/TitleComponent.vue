accdb020608453efb296710a1879a88786866574
Radek011200 <beziek120@o2.pl>
2022-04-29 10:32:49 +0200

    Dodanie komponentu tytulu

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="head-title"> {{title}}</h1>
+    <v-divider>inset</v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TitleComponent",
+  props: {
+    title: {
+      type: String,
+      default: "Title"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
