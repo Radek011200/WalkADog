0aeb70c127866313e11334ba6ec388f3073fc2d3
Radek011200 <beziek120@o2.pl>
2022-05-17 17:56:35 +0200

    add axios

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import pl from 'vuetify/src/locale/pl.ts'
 import Vuetify from 'vuetify/lib'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
@@ -16,32 +16,25 @@ Vue.config.productionTip = false
 library.add(fas)
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+    router,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
 
 Vue.use(Vuetify).component('fa', FontAwesomeIcon)
 
 Vue.component('my-component', {
-  methods: {
-    changeLocale () {
-      this.$vuetify.lang.current = 'pl'
+    methods: {
+        changeLocale() {
+            this.$vuetify.lang.current = 'pl'
+        },
     },
-  },
 })
 
 export default new Vuetify({
-  lang: {
-    locales: { pl },
-    current: 'pl',
-  },
+    lang: {
+        locales: {pl},
+        current: 'pl',
+    },
 })
-export const HTTP = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  headers: {
-    Authorization: 'Bearer {token}',
-    xsrfCookieName: 'XSRF-TOKEN',
-    xsrfHeaderName: 'X-XSRF-TOKEN',
-  }
-})
+Vue.prototype.$http = axios;
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-  <TitleComponent :title="title" />
-  <v-card-actions class="justify-center">
-
-    <v-btn href="/new-walk" color="success" :x-large=true rounded>Zaplanuj spacer</v-btn>
-  </v-card-actions>
+    <TitleComponent :title="title"/>
+    <v-card-actions class="justify-center">
+      <v-btn href="/new-walk" color="success" :x-large=true rounded>Zaplanuj spacer</v-btn>
+    </v-card-actions>
   </div>
 </template>
 
 <script>
-import { HTTP } from '@/main.js';
+
 import TitleComponent from "@/components/TitleComponent";
 
 export default {
@@ -18,19 +17,8 @@ export default {
   data() {
     return {
       title: "Strona główna",
-      posts: [],
     };
   },
-  mounted () {
-    HTTP.get(`users`)
-        .then(response => {
-          this.posts = response.data
-        })
-        .catch(e => {
-          this.errors.push(e)
-        })
-
-  }
 }
 </script>
 
