587c1959881396547b8c1e947233bf6088231184
Radek011200 <beziek120@o2.pl>
2022-05-19 00:25:42 +0200

    add login form and view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <LoginForm/>
+  </div>
+</template>
+
+<script>
+import LoginForm from "@/components/LoginForm.vue";
+
+export default {
+  name: "LoginView.vue",
+  components: {LoginForm},
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  methods: {
+    // login() {
+    //   if (this.email !== "" && this.password !== "") {
+    //     this.$emit('login', {
+    //       email: this.email,
+    //       password: this.password,
+    //       access_token: 1,
+    //     });
+    //     this.$router.push('/');
+    //     this.access_token = 1;
+    //   }
+    // }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
