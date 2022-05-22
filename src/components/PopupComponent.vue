b0a37cdc024d0e666464af5110b94c866e73b9dc
Radek011200 <beziek120@o2.pl>
2022-05-22 21:11:12 +0200

    add popup component

---
+++ src/components/PopupComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div :class="{ modal: 'modal', 'is-active': isActive}">
+      <div class="modal-background" @click="$emit('close-modal')"></div>
+      <div class="modal-card rounded">
+        <section class="modal-card-body is-active">
+          <slot name="content"></slot>
+          <v-row align="center" justify="center" dense>
+            <button @click="closeModal" class="button is-success mt-6" align="center">{{ confirmButtonTitle }}</button>
+          </v-row>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopupComponent",
+  props: {
+    modalTitle: {
+      type: String,
+      default: 'Tytuł',
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    confirmButtonTitle: {
+      type: String,
+      default: 'Zatwierdź',
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close-modal');
+
+    },
+
+  }
+}
+</script>
+
+<style scoped>
+@media screen and (max-width: 776px) {
+  .modal-card {
+    width: 80%;
+  }
+}
+
+</style>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <TitleComponent :title="title"></TitleComponent>
-    <v-container fluid>
-      <v-row align="center" justify="center" dense>
-        <v-btn color="success" @click="toggleModal" :large=true rounded> Wyloguj</v-btn>
-      </v-row>
-    </v-container>
+    <v-btn color="success" @click="togglePopup" :large=true rounded> Powiadomienia</v-btn>
+    <v-btn color="success" @click="toggleModal" :large=true rounded> Wyloguj</v-btn>
     <ModalComponent
         @confirm="logoutAction"
         @close-modal="toggleModal"
@@ -17,16 +14,28 @@
       <!--      Przykładowe użycia slota-->
       <!--      <template v-slot:content>Coś w środku modala</template>-->
     </ModalComponent>
+    <PopupComponent
+        @close-modal="togglePopup"
+        :is-active="isActivePopup"
+        :confirm-button-title="closePopup">
+      Przykładowe użycia slota
+      <template v-slot:content>
+        <div class="popup-text">
+          {{ notifications }}
+        </div>
+      </template>
+    </PopupComponent>
   </div>
 </template>
 
 <script>
 import ModalComponent from "@/components/ModalComponent";
 import TitleComponent from "@/components/TitleComponent";
+import PopupComponent from "@/components/PopupComponent";
 
 export default {
   name: "AccountView",
-  components: {ModalComponent, TitleComponent},
+  components: {PopupComponent, ModalComponent, TitleComponent},
   data() {
     return {
       isActive: false,
@@ -34,11 +43,17 @@ export default {
       confirmButtonTitle: 'Tak, wyloguj',
       cancelButtonTitle: 'Nie, wróć',
       title: 'Moje konto',
+      isActivePopup: false,
+      closePopup: 'Zamknij',
+      notifications: 'Brak powiadomień',
     };
   },
   methods: {
     toggleModal() {
       this.isActive = !this.isActive;
+    },
+    togglePopup() {
+      this.isActivePopup = !this.isActivePopup;
     },
     logoutAction() {
       localStorage.removeItem("token");
@@ -50,5 +65,25 @@ export default {
 
 
 <style scoped>
+.popup-text {
+  font-size: 1.4rem;
+  text-align: center;
+}
 
+.v-btn {
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
+  padding-bottom: 100px;
+  margin-bottom: 1.5rem;
+
+}
+
+@media screen and (min-width: 600px) {
+  .v-btn {
+    width: 60%;
+    margin-left: 20%;
+    margin-right: 20%;
+  }
+}
 </style>
