09c4dc99a1aad00bde7d55248a7b5bff52dd24fa
Filip <filipgcs@gmail.com>
2022-05-28 23:54:05 +0200

    unfinished trainer profile

---
+++ src/components/TrainerProfileComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <v-container>
+      <v-layout>
+        <v-flex>
+         
+            <v-card-text>
+
+                        <div class="profile">
+                            <v-icon size="200px">mdi-account-box</v-icon>
+                            <ul style="list-style-type: none;">
+                                <li><StarRating increment="1" :rating="rating" active-color="black" read-only="True"></StarRating></li>
+                                <li>{{name}}</li>
+                                <li>{{address}}</li>
+                                <li>{{mobile}}</li>
+                            </ul>
+                        </div>
+            <v-btn class="center" href="#" color="success" :large=true rounded>
+                    Opinie
+            </v-btn>
+            </v-card-text>
+            <v-card-actions class="lista">
+                <v-spacer></v-spacer>
+            </v-card-actions>
+
+        </v-flex>
+      </v-layout>
+      </v-container>
+  </div>
+</template>
+
+<script>
+
+
+import StarRating from 'vue-star-rating';
+
+export default {
+  name: "TrainerProfile",
+  components: {
+    StarRating
+  },
+  props: {
+    name: {
+      type: String,
+      default: "Trainer Name"
+    },
+    mobile: {
+      type: String,
+      default: "Mobile"
+    },
+    address: {
+      type: String,
+      default: "Address"
+    },
+    rating: {
+      type: String,
+      default: '5'
+    },
+  }
+}
+</script>
+
+<style>
+
+.profile {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+  text-align: center;
+}
+.center{
+  display: flex;
+  align-items: center;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+}
+.vue-star-rating{
+    padding: 10px 0 10px 0;
+    justify-content: center;
+}
+</style>
---
+++ src/views/TrainerProfile.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+<TitleComponent :title="title"></TitleComponent>
+
+<v-list-item v-for="trainer in getTreners" :key="trainer.id" dense>
+  <v-list-item-content class="lista">
+    <TrainerProfileComponent :rating="trainer.rating" :name="trainer.name" :address="trainer.address" :mobile="trainer.mobile"></TrainerProfileComponent>
+  </v-list-item-content>
+  </v-list-item>
+</div>
+</template>
+
+<script>
+
+  import TitleComponent from "@/components/TitleComponent";
+  import TrainerProfileComponent from "@/components/TrainerProfileComponent";
+
+
+
+  export default {
+    name: 'TrainersList',
+    components: {
+    TitleComponent,
+    TrainerProfileComponent,
+},
+    computed: {
+      getTreners() {
+        return this.trainers;
+      },
+
+    },
+    created() {
+      this.title = "Profil Trenera";
+    },
+
+    data() {
+      return {
+        title: 'Profil Trenera',
+        trainers: [
+          {
+            id: 1,
+            name: 'Kajtek',
+            mobile: '156-123-123',
+            address: 'Olsztyn ul.Słoneczna 54',
+            rating: 4,
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+
+.lista {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px;
+}
+
+</style>
