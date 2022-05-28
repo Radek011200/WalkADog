61217375a4e74618427ccf105aa6243c9c30d5ab
Filip <filipgcs@gmail.com>
2022-05-28 23:54:46 +0200

    unfinished planned walks

---
+++ src/components/PlannedWalks.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card class="walk" outlined>          
+            <v-card-text>
+                <v-container>
+                        <div class="lista">
+                            <p><b>{{dog_name}}</b></p>
+                            <p>{{start_hour}}</p>
+                            <p>{{end_hour}}</p>
+                            <v-icon size="50px">mdi-information-variant</v-icon>
+                            <v-icon size="50px">mdi-map</v-icon>
+                        </div>
+                </v-container>
+            </v-card-text>
+            <v-card-actions class="lista">
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      </v-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "TrainerEntry",
+  props: {
+    dog_name: {
+      type: String,
+      default: "Dog Name"
+    },
+    start_hour: {
+      type: String,
+      default: "Start Hour"
+    },
+    end_hour: {
+      type: String,
+      default: "End Hour"
+    },
+  }
+}
+</script>
+
+<style>
+
+.walk{
+    width: 50%;
+    height: 70%;
+    display: inline;
+    justify-content: center;
+}
+
+.lista {
+    width: 50%;
+}
+
+</style>
---
+++ src/views/YourWalks.vue
@@ -0,0 +1,108 @@
+<template>
+<div>
+<TitleComponent :title="title"></TitleComponent>
+<Calendar></Calendar>
+<h2>Zaplanowane Spacery</h2>
+<PlannedWalks></PlannedWalks>
+<v-list-item v-for="walk in this.walks" :key="walk.url" dense>
+  <v-list-item-content class="lista">
+      <PlannedWalks :id="walk.id" :dog_name="walk.dog.name" :start_hour="walk.start_hour" :end_hour="walk.end_hour"></PlannedWalks>
+  </v-list-item-content>
+  </v-list-item>
+      <v-btn class="center" href="/walk-list" color="success" :large=true rounded>
+                          Pokaż szczegóły
+                  </v-btn>
+      <v-btn class="center" text :x-large=true @click="back()">Powrót</v-btn>
+</div>
+</template>
+
+<script>
+
+  import TitleComponent from "@/components/TitleComponent";
+  import Calendar from "@/components/Calendar";
+  import PlannedWalks from "@/components/PlannedWalks";
+  import axios from 'axios';
+
+
+  export default {
+    name: 'YourWalks',
+    components: {
+      TitleComponent,
+      PlannedWalks,
+      Calendar
+    },
+
+    
+
+    created() {
+      this.title = "Twoje Spacery";
+      this.getToken();
+      this.getWalks();
+    },
+
+    data() {
+      return {
+        title: 'Lista Spacerów',
+        walks: []
+      }
+    },
+
+    methods: {
+        back() {
+      this.$router.go(-1)
+    },
+      getToken() {
+     axios({
+            method: 'post',
+            url: 'http://localhost:8000/api-token-auth/',
+            data: {
+              username: 'admin',
+              password: 'admin'
+            }
+        }).then(response => localStorage.setItem("token",response.data.token ));
+    },
+
+    getWalks() {
+      axios({
+            method: 'get',
+            url: 'http://localhost:8000/api/walks/',
+            headers: {
+              Authorization: 'Token ' + localStorage.token
+            }
+        }).then(response => this.walks = response.data.results);
+        }
+    },
+
+     getDetails(url) {
+      let result = '';
+      axios({
+            method: 'get',
+            url: url,
+            headers: {
+              Authorization: 'Token ' + localStorage.token
+            }
+        }).then(response => result = response.data.result);
+        return result;
+    },
+    }
+  
+</script>
+
+<style>
+
+.lista {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px;
+}
+h2{
+    padding-left: 5%;
+}
+.center{
+    width: 40%;
+    margin: auto;
+}
+
+</style>
